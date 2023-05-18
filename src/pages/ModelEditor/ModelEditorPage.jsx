157ed2aed5ff54da9e30e499d3dc149d4d87acb0
Farazzaidi22 <farazkhalid05@yahoo.com>
2023-05-18 23:29:03 +0500

    added toast

--- src/pages/ModelEditor/ModelEditorPage.jsx
+++ src/pages/ModelEditor/ModelEditorPage.jsx
@@ -1,128 +1,136 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ModelViewer from "../../components/ModalEditor/ModelViewer";
 import ColorWheel from "../../components/ColorWheel/ColorWheel";
 
 import { FaBatteryQuarter, FaRegArrowAltCircleRight } from "react-icons/fa";
 import { BsBroadcast } from "react-icons/bs";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export const ModelEditorPage = () => {
+  useEffect(() => {
+    toast("🦄 Wow so easy!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }, []);
+
   return (
-    <div
-      style={{
-        width: "100%",
-      }}
-    >
+    <>
+      <ToastContainer />
       <div
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center" /* Add this line */,
-          position: "fixed" /* Add this line */,
-          top: 0 /* Add this line */,
-          left: 0 /* Add this line */,
-          width: "100%" /* Add this line */,
-          padding: "5px" /* Adjust the padding value as needed */,
-
-          // border: "2px solid red",
+          width: "100%",
         }}
       >
         <div
           style={{
             display: "flex",
-            justifyContent: "center",
+            justifyContent: "space-between",
             alignItems: "center" /* Add this line */,
-            width: "20%",
+            position: "fixed" /* Add this line */,
+            top: 0 /* Add this line */,
+            left: 0 /* Add this line */,
+            width: "100%" /* Add this line */,
+            padding: "5px" /* Adjust the padding value as needed */,
+
+            // border: "2px solid red",
           }}
         >
-          <img
-            src="/logo/logo.jpg"
-            alt="Logo"
+          <div
             style={{
-              width: "100%",
-              height: "auto",
-            }} /* Adjust the image size as needed */
-          />
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center" /* Add this line */,
+              width: "20%",
+            }}
+          >
+            <img
+              src="/logo/logo.jpg"
+              alt="Logo"
+              style={{
+                width: "100%",
+                height: "auto",
+              }} /* Adjust the image size as needed */
+            />
+          </div>
         </div>
-        <h3
+
+        <h5
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center" /* Add this line */,
-            width: "80%",
-            margin: 0 /* Add this line to remove default margin */,
+            padding: "2%",
+            textAlign: "left",
+            width: "50%",
+            marginTop: "8%",
+            // border: "1px solid red",
           }}
         >
-          Alert and confirmation
-        </h3>
+          Person Name, Section Name. User Id and Micro controller id
+        </h5>
+
+        <div
+          style={{
+            // border: "2px solid blue",
+
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              // border: "2px solid black",
+
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <BsBroadcast style={{ marginBottom: "300px" }} color="white" />
+          </div>
+
+          <div
+            style={{
+              // border: "2px solid green",
+
+              height: "50vh",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <ModelViewer
+              position={[0, 0, 0]}
+              scale={0.2}
+              modelPath={"/3dModels/Breathing Idle.glb"}
+            />
+          </div>
+
+          <div
+            style={{
+              // border: "2px solid black",
+
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <FaBatteryQuarter color="white" />
+
+            <FaRegArrowAltCircleRight
+              style={{ marginBottom: "200px" }}
+              color="white"
+            />
+          </div>
+        </div>
+
+        <ColorWheel />
       </div>
-
-      <h5
-        style={{
-          padding: "2%",
-          textAlign: "left",
-          width: "50%",
-          marginTop: "8%",
-          // border: "1px solid red",
-        }}
-      >
-        Person Name, Section Name. User Id and Micro controller id
-      </h5>
-
-      <div
-        style={{
-          // border: "2px solid blue",
-
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div
-          style={{
-            // border: "2px solid black",
-
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <BsBroadcast style={{ marginBottom: "300px" }} color="white" />
-        </div>
-
-        <div
-          style={{
-            // border: "2px solid green",
-
-            height: "50vh",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <ModelViewer
-            position={[0, 0, 0]}
-            scale={0.2}
-            modelPath={"/3dModels/Breathing Idle.glb"}
-          />
-        </div>
-
-        <div
-          style={{
-            // border: "2px solid black",
-
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <FaBatteryQuarter color="white" />
-
-          <FaRegArrowAltCircleRight
-            style={{ marginBottom: "200px" }}
-            color="white"
-          />
-        </div>
-      </div>
-
-      <ColorWheel />
-    </div>
+    </>
   );
 };
